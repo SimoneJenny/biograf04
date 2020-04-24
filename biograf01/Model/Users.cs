ca4c40f270739fcdf41e166047951c1d4b375765
Simone Hansen <simone.hansen@siemensgamesa.com>
2020-04-24 14:43:51 +0200

    small changes

--- biograf01/Model/Users.cs
+++ biograf01/Model/Users.cs
@@ -9,17 +9,14 @@ namespace biograf01.Model
 {
     public class Users
     {
-        [Key]
         public int usersId { get; set; } //skal ligne klasse...//primary key
         public string username { get; set; }
         public string password { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
         public string mailAddress { get; set; }
-
-        [ForeignKey("zipcodeId")] 
         public int ZipCodeId { get; set; }
-        public ZipCode Zipcode { get; set; } //en user til en zipcod
+        public ZipCode Zipcode { get; set; } //en user til en zipcod //FK
         public IList<UserZipCode> User { get; set; } ///???
     }
 }
